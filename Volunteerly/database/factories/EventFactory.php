ca8810088a6952788ab35416b10dd8709734fd9a
Littery <93970315+Littery1@users.noreply.github.com>
2024-03-21 16:41:05 +0100

    merge MapActions branch

--- Volunteerly/database/factories/EventFactory.php
+++ Volunteerly/database/factories/EventFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Event;
+use App\Models\Location;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
