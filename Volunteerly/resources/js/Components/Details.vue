956f97864fedf68627d1182ec7935441153b5420
Littery <93970315+Littery1@users.noreply.github.com>
2024-04-27 13:52:31 +0200

    bind page-id

--- Volunteerly/resources/js/Components/Details.vue
+++ Volunteerly/resources/js/Components/Details.vue
@@ -1,6 +1,37 @@
 <template>
+    <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24 relative">
+        <div class="flex flex-col items-center">
+            <div class="border-b border-gray-400 w-full mb-4"></div>
+
+            <div class="flex  ml-[-33vw]">
+                <!-- First h2 element -->
+                <div class="flex-none">
+                    <h2 class="text-2xl font-bold text-blue-700 cursor-pointer"
+                        @click="() => VueScrollTo.scrollTo('#description')">
+                        Popis</h2>
+                </div>
+
+                <!-- Second h2 element -->
+                <div class="flex-none ml-4 text-blue-700">
+                    <h2 class="text-2xl font-bold cursor-pointer" 
+                    @click="() => VueScrollTo.scrollTo('#map')">Mapa</h2>
+                </div>
+
+                <!-- Third h2 element -->
+                <div class="flex-none ml-4 text-blue-700 ">
+                    <h2 class="text-2xl font-bold cursor-pointer" 
+                    @click="() => VueScrollTo.scrollTo('#rating')">
+                        Hodnocení</h2>
+                </div>
+            </div>
+
+            <div class="border-b border-gray-400 w-full mt-4"></div>
+        </div>
+    </div>
+
     <div class="ml-[15vw]">
         <div style="width: 75vw;">
+
 
 
             <div class="flex items-center m-5">
@@ -9,18 +40,18 @@
             </div>
             <Calendar :event="event" />
 
-            <div class="flex items-center m-5">
+            <div class="flex items-center m-5" id="description">
                 <fwb-img class="w-10" src="/images/DocumentLogo.png" />
                 <h2 class="text-xl ml-2 text-blue-500 font-bold">Popis:</h2>
             </div>
             <p class="text-sm m-6">{{ event.data.description }}</p>
-            <ShowEvent :event="event" />
+            <ShowEvent :event="event" id="map" />
 
-            <div class="flex items-center m-5 ">
+            <div class="flex items-center m-5  " id="rating">
                 <fwb-img class="w-20" src="/images/Rating.png" />
                 <h2 class="text-xl ml-2 text-blue-500 font-bold">Hodnocení:</h2>
             </div>
-            <Comments page-language="cs" :website-id="11002" page-id="currentUrl" />
+            <Comments page-language="cs" :website-id="11002" :page-id="currentUrl" />
 
         </div>
 
@@ -34,6 +65,7 @@ import { FwbImg } from 'flowbite-vue';
 import ShowEvent from '@/Components/Maps/ShowEvent.vue';
 import { Comments } from '@hyvor/hyvor-talk-vue';
 import Calendar from '@/Components/Calendar.vue';
+import VueScrollTo from 'vue-scrollto';
 
 
 const currentUrl = window.location.href;
--- Volunteerly/resources/js/Pages/Events/Show.vue
+++ Volunteerly/resources/js/Pages/Events/Show.vue
@@ -4,32 +4,8 @@
         <div>
             <h1 class="text-blue-600 text-4xl text-center font-bold ">{{ event.data.name }}</h1>
 
-            <Gallery class=" flex items-center min-h-screen" style="transform: scale(0.8);" />
+            <Gallery class=" flex items-center min-h-screen" style="transform: scale(0.8);" />       
 
-                <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24 relative">
-                    <div class="flex flex-col items-center">
-                        <div class="border-b border-gray-400 w-full mb-4"></div>
-
-                        <div class="flex  ml-[-33vw]">
-                            <!-- First h2 element -->
-                            <div class="flex-none">
-                                <h2 class="text-2xl font-bold">Popis</h2>
-                            </div>
-
-                            <!-- Second h2 element -->
-                            <div class="flex-none ml-4">
-                                <h2 class="text-2xl font-bold">Mapa</h2>
-                            </div>
-
-                            <!-- Third h2 element -->
-                            <div class="flex-none ml-4">
-                                <h2 class="text-2xl font-bold">Hodnocení</h2>
-                            </div>
-                        </div>
-
-                        <div class="border-b border-gray-400 w-full mt-4"></div>
-                    </div>
-                </div>                
                 <Details :event="event" />
 
         </div>
